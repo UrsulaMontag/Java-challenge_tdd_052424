105aaaaa4326449b553bf325fcaee0046aad8411
ugla <montagu666@gmail.com>
2024-05-24 16:15:35 +0200

    Refactoring of getX

--- src/main/java/org/example/PlayerCharacter.java
+++ src/main/java/org/example/PlayerCharacter.java
@@ -2,14 +2,14 @@ package org.example;
 
 public class PlayerCharacter {
     static int y = 0;
-    int x = 0;
+    static int x = 0;
 
     public static void main(String[] args) {
 
     }
 
     public static int getX() {
-        return 0;
+        return x;
     }
 
     public static int getY() {
