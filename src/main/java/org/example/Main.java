4114032e74aaac581887b4bf4aa1cef6d6c21394
ugla <montagu666@gmail.com>
2024-05-24 15:33:12 +0200

    Describe task and setup testing environment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+
+    /**
+     * 1. Create a 2D grid
+     * 2. Crate game character
+     * 3. move up using W
+     * 4. move down using S
+     * 5. move right using D
+     * 6. move left using A
+     * */
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
     }
 }
